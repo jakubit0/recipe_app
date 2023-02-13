c4233b1855a5dc78ccf1bc3a0c1b24d08f863ae3
jakubit0 <kubaleszczyniak@gmail.com>
2023-02-13 20:23:51 +0100

    Created GestureDetector in main

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
             itemCount: Recipe.samples.length,
             itemBuilder: (BuildContext context, int index) {
-              //TODO: Add GestureDetector
-              return buildRecipeCard(Recipe.samples[index]);
+              return GestureDetector(
+                onTap: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) {
+                          //TODO: Replace return with return RecipeDetail()
+                          return const Text('Detail Page');
+                        },
+                    ),
+                );
+                },
+              );
             }
             ),
       ),
