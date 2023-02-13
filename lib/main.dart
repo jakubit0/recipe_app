8a1d665d51a56e3e149097bd04b29ff3dfdd1f3b
jakubit0 <kubaleszczyniak@gmail.com>
2023-02-13 14:18:30 +0100

    Updated main.dart
    Cleaned main and added SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class RecipeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+
     final ThemeData theme = ThemeData();
     return MaterialApp(
       title: 'Recipe Calculator',
@@ -35,15 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
 
@@ -52,27 +43,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: SafeArea(
+        // TODO: Replace child: Container()
+        //
+        child: Container(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), //
     );
   }
+  //TODO: Add buildRecipeCard() here
 }
