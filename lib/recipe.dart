df4d3d3a3b66500945aa8c28efcc947779bc3070
jakubit0 <kubaleszczyniak@gmail.com>
2023-02-13 19:44:55 +0100

    Fixed path with assets
    in pubspec.yaml added assets and changed path in recipe class

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -10,27 +10,27 @@ class Recipe{
   static List<Recipe> samples = [
     Recipe(
     'Spaghetti and MeatBalls',
-    'assets/2126711929_ef763de2b3_w.jpg'
+    'assets/images/2126711929_ef763de2b3_w.jpg'
     ),
     Recipe(
         'Tomato Soup',
-        'assets/3187380632_5056654a19_b.jpg'
+        'assets/images/3187380632_5056654a19_b.jpg'
     ),
     Recipe(
         'Grilled Cheese',
-        'assets/8533381643_a31a99e8a6_c.jpg'
+        'assets/images/8533381643_a31a99e8a6_c.jpg'
     ),
     Recipe(
         'Chocolate Chip Cookies',
-        'assets/15452035777_294cefced5_c.jpg'
+        'assets/images/15452035777_294cefced5_c.jpg'
     ),
     Recipe(
         'Taco Saland',
-        'assets/15992102771_b92f4cc00a_b.jpg'
+        'assets/images/15992102771_b92f4cc00a_b.jpg'
     ),
     Recipe(
         'Hawaiian Pizza',
-        'assets/27729023535_a57606c1be.jpg'
+        'assets/images/27729023535_a57606c1be.jpg'
     ),
   ];
 }
